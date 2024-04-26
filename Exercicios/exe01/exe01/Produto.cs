436e52193cb583ea23d5b654c3333797454b3ee5
Letícia Nascimento de Almeida <124791668+LeticiaAlmeida16@users.noreply.github.com>
2024-04-26 12:45:56 -0300

    Add files via upload

---
+++ Exercicios/exe01/exe01/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe01
+{
+    public class Produto
+    {
+        private string Nome { get; set; }
+        private double Preco { get; set; }
+        private int QntdEmEstoque { get; set; }
+
+        public Produto(string nome, double preco, int qntdEmEstoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            QntdEmEstoque = qntdInicial;
+        }
+
+        public void AdicionarEstoque(int )
+    }
+}
---
+++ Exercicios/exe01/exe01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exe01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
