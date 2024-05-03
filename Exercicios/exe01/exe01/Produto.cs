eedc43f679a619e424a2e15584f3d32b1a273a68
Letícia Nascimento de Almeida <124791668+LeticiaAlmeida16@users.noreply.github.com>
2024-05-03 12:01:21 -0300

    Add files via upload

--- Exercicios/exe01/exe01/Produto.cs
+++ Exercicios/exe01/exe01/Produto.cs
@@ -8,17 +8,58 @@ namespace exe01
 {
     public class Produto
     {
-        private string Nome { get; set; }
-        private double Preco { get; set; }
-        private int QntdEmEstoque { get; set; }
+        // Atributos
 
-        public Produto(string nome, double preco, int qntdEmEstoque)
+        private string nome;
+        private double preco;
+        private int quantidadeEmEstoque;
+
+        // Construtor
+        public Produto(string nome, double preco, int quantidadeInicial)
         {
-            Nome = nome;
-            Preco = preco;
-            QntdEmEstoque = qntdInicial;
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = quantidadeInicial;
+        }
+        // Método para adicionar itens ao estoque
+        public void AdicionarEstoque(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                quantidadeEmEstoque += quantidade;
+                Console.WriteLine($"{quantidade} unidades do produto '{nome}' foram adicionadas ao estoque.");
+            }
+            else
+            {
+                Console.WriteLine("A quantidade a ser adicionada deve ser maior que zero.");
+            }
         }
 
-        public void AdicionarEstoque(int )
+        // Método para remover itens do estoque
+        public void RemoverEstoque(int quantidade)
+        {
+            if (quantidade > 0 && quantidade <=  quantidadeEmEstoque)
+            {
+                quantidadeEmEstoque -= quantidade;
+                Console.WriteLine($"{quantidade} unidades do produto '{nome}' foram removidas do estoque.");
+            }
+            else if (quantidade > quantidadeEmEstoque)
+            {
+                Console.WriteLine($"Não há {quantidade} unidades do produto '{nome}' disponíveis no estoque.");
+            }
+            else
+            {
+                Console.WriteLine("A quantidade a ser removida deve ser maior que zero.");
+            }
+        }
+
+        // Método para exibir informações do produto
+        public void MostrarInformacoes()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Preço: R${preco:F2}");
+            Console.WriteLine($"Quantidade em estoque: { quantidadeEmEstoque} ");
+
+        }
     }
 }
